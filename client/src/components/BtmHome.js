98cdcd83191818847c9a88caf30626c3bb35fba4
abdulhakim-altunkaya <altunkaya2@gmail.com>
2025-02-28 00:27:35 +0300

    more improvements on the main page

--- client/src/components/BtmHome.js
+++ client/src/components/BtmHome.js
@@ -13,53 +13,96 @@ function BottomHome() {
 
       <div className='infoAreas2'>
         <div className='infoSheets2' id='companyBox'>
-          <span className='infoSVG2'><img src='/svg_translation.svg' alt='Translation icon'/></span>
-          <span className='infoTitle2'>Drēbes, apavi</span>
-          <span className='infoText2'>Alanında tecrübeli Letonya'da yaşayan tercümanlarımız 
-            gerek yazılı gerek sözlü Letonca-Rusça-Türkçe-İngilizce dillerinde çeviri hizmetleri sunmaktadırlar.
-            Letonya'daki tercümanlarımız aynı zamanda Konsolosluk ve Noterliklere de yeminli tercüman olarak
-            hizmet etmektedirler. 
-          </span>
+          <span className='infoSVG2'><img src='/svg_machine.svg' alt='Machine and Construction icon'/></span>
+          <span className='infoTitle2'>Mašīnas, būvniecība</span>
+          <div className='infoText2'>
+            <span>Būvmateriāli</span>
+            <span>Santehnika</span>
+            <span>Dārza tehnika</span>
+            <span>Pārvadāšana un iekraušana</span>
+            <span>Ģeneratori</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Mērinstrumenti</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Un vēl...</span>
+          </div>
         </div>
         <div className='infoSheets2' id='companyBox2'>
-          <span className='infoSVG2'><img src='/svg_tourist.svg' alt='Tourist icon'/></span>
-          <span className='infoTitle2'>Mājai</span>
-          <span className='infoText2'>İşadamı ve bürokrat heyetlerimiz, turistik kafileler ve vatandaşlarımıza Letonya'ya 
-            yapacakları seyahat boyunca tecrübeli ve dil bilen rehberlerimiz hizmet etmektedir. Rehberlerimiz 
-            Letonya, Estonya, Litvanya, Baltıklar ve İskandinav ülkelerinde görev alabilmektedir.  
+          <span className='infoSVG2'><img src='/svg_laptop.svg' alt='Instruments and Electronics icon'/></span>
+          <span className='infoTitle2'>Instrumenti, elektronika</span>
+          <span className='infoText2'>
+            <span>Būvmateriāli</span>
+            <span>Santehnika</span>
+            <span>Dārza tehnika</span>
+            <span>Pārvadāšana un iekraušana</span>
+            <span>Ģeneratori</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Mērinstrumenti</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Un vēl...</span>
           </span>
         </div>
         <div className='infoSheets2' id='companyBox3' onClick={ () => navigate("/sirket-firma-kurulus")}>
-          <span className='infoSVG2'><img src='/svg_company.svg' alt='Company icon'/></span>
-          <span className='infoTitle2'>Iekārta</span>
-          <span className='infoText2'>Letonya'da şirket kurmak isteyen vatandaşlarımıza bu alanda oldukça tecrübeli personelimiz
-            hizmet sunmakta olup, detaylı bilgi için tıklayabilirsiniz.
+          <span className='infoSVG2'><img src='/svg_car2.svg' alt='Car icon'/></span>
+          <span className='infoTitle2'>Transportlīdzekļi</span>
+          <span className='infoText2'>
+            <span>Būvmateriāli</span>
+            <span>Santehnika</span>
+            <span>Dārza tehnika</span>
+            <span>Pārvadāšana un iekraušana</span>
+            <span>Ģeneratori</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Mērinstrumenti</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Un vēl...</span>
           </span>
         </div>
       </div>
 
       <div className='infoAreas2'>
         <div className='infoSheets2' id='companyBox2'>
-          <span className='infoSVG2'><img src='/svg_agriculture.svg' alt='Agriculture icon'/></span>
-          <span className='infoTitle2'>Lauksaimniecība</span>
-          <span className='infoText2'>Şirketinizin ihtiyaç duyacağı banka hesabı açma, muhasebe hizmetleri, insan kaynakları, 
-            ihalelere katılım, ticari alan kiralama, reklam, pazar araştırması ve diğer teknik konularda 
-            detaylı hizmetler sunmaktayız.
+          <span className='infoSVG2'><img src='/svg_dress.svg' alt='Dress and shoes icon'/></span>
+          <span className='infoTitle2'>Apģērbi, apavi</span>
+          <span className='infoText2'>
+            <span>Būvmateriāli</span>
+            <span>Santehnika</span>
+            <span>Dārza tehnika</span>
+            <span>Pārvadāšana un iekraušana</span>
+            <span>Ģeneratori</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Mērinstrumenti</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Un vēl...</span>
           </span>
         </div>
         <div className='infoSheets2' id='companyBox'>
-          <span className='infoSVG2'><img src='/svg_hobby.svg' alt='Hobby icon'/></span>
-          <span className='infoTitle2'>Atpūta, hobiji</span>
-          <span className='infoText2'>Şirketlerimiz ve vatandaşlarımızın Letonya'da ihtiyaç duyacakları Konsolosluk,
-            Noterlik belge onayları, apostil işlemleri, randevu takipleri, yabancı kurumların aranması gibi konularda 
-            dil bilen personelimiz ile hizmet sunmaktayız.
+          <span className='infoSVG2'><img src='/svg_baby.svg' alt='Baby icon'/></span>
+          <span className='infoTitle2'>Bērni</span>
+          <span className='infoText2'>
+            <span>Būvmateriāli</span>
+            <span>Santehnika</span>
+            <span>Dārza tehnika</span>
+            <span>Pārvadāšana un iekraušana</span>
+            <span>Ģeneratori</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Mērinstrumenti</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Un vēl...</span>
           </span>
         </div>
         <div className='infoSheets2' id='companyBox2'>
-          <span className='infoSVG2'><img src='/svg_customs1.svg' alt='Customs, tax, accounting icon'/></span>
-          <span className='infoTitle2'>Gümrük, Vergi</span>
-          <span className='infoText2'>Letonya gümrüklerinden mal çekme ve vergi işlemleri konularında uzmanlarımızla 
-            kurumsal hizmetler sunmaktayız.
+          <span className='infoSVG2'><img src='/svg_home.svg' alt='Home and Hobbies icon'/></span>
+          <span className='infoTitle2'>Māja, hobijs</span>
+          <span className='infoText2'>
+            <span>Būvmateriāli</span>
+            <span>Santehnika</span>
+            <span>Dārza tehnika</span>
+            <span>Pārvadāšana un iekraušana</span>
+            <span>Ģeneratori</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Mērinstrumenti</span>
+            <span>Mazgāšanas aprīkojums</span>
+            <span>Un vēl...</span>
           </span>
         </div>
       </div>
