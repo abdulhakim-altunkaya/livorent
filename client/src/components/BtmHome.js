dff11a50195f120b0afb3dc22f59de2e984e0984
abdulhakim-altunkaya <altunkaya2@gmail.com>
2025-03-01 22:45:55 +0300

    main page further improved

--- client/src/components/BtmHome.js
+++ client/src/components/BtmHome.js
@@ -18,7 +18,7 @@ function BottomHome() {
           <div className='infoText2'>
             <span>Būvmateriāli</span>
             <span>Santehnika</span>
-            <span>Dārza tehnika</span>
+            <span>Kompresori</span>
             <span>Pārvadāšana un iekraušana</span>
             <span>Ģeneratori</span>
             <span>Mazgāšanas aprīkojums</span>
@@ -31,14 +31,14 @@ function BottomHome() {
           <span className='infoSVG2'><img src='/svg_laptop.svg' alt='Instruments and Electronics icon'/></span>
           <span className='infoTitle2'>Instrumenti, elektronika</span>
           <span className='infoText2'>
-            <span>Būvmateriāli</span>
-            <span>Santehnika</span>
-            <span>Dārza tehnika</span>
-            <span>Pārvadāšana un iekraušana</span>
-            <span>Ģeneratori</span>
-            <span>Mazgāšanas aprīkojums</span>
-            <span>Mērinstrumenti</span>
-            <span>Mazgāšanas aprīkojums</span>
+            <span>Telefoni</span>
+            <span>Datori</span>
+            <span>Virtuves tehnika</span>
+            <span>Orgtehnika</span>
+            <span>Baterijas, Akumulatori</span>
+            <span>Televizori</span>
+            <span>Foto un optika</span>
+            <span>Mūzikas instrumenti</span>
             <span>Un vēl...</span>
           </span>
         </div>
@@ -46,14 +46,14 @@ function BottomHome() {
           <span className='infoSVG2'><img src='/svg_car2.svg' alt='Car icon'/></span>
           <span className='infoTitle2'>Transportlīdzekļi</span>
           <span className='infoText2'>
-            <span>Būvmateriāli</span>
-            <span>Santehnika</span>
-            <span>Dārza tehnika</span>
-            <span>Pārvadāšana un iekraušana</span>
-            <span>Ģeneratori</span>
-            <span>Mazgāšanas aprīkojums</span>
-            <span>Mērinstrumenti</span>
-            <span>Mazgāšanas aprīkojums</span>
+            <span>Vieglie auto</span>
+            <span>Velosipēdi, skūteri</span>
+            <span>Kravas automašīnas</span>
+            <span>Traktori</span>
+            <span>Lauksaimniecības mašīnas</span>
+            <span>Piekabes</span>
+            <span>Jumta kastes</span>
+            <span>Ūdens transports</span>
             <span>Un vēl...</span>
           </span>
         </div>
@@ -64,14 +64,14 @@ function BottomHome() {
           <span className='infoSVG2'><img src='/svg_dress.svg' alt='Dress and shoes icon'/></span>
           <span className='infoTitle2'>Apģērbi, apavi</span>
           <span className='infoText2'>
-            <span>Būvmateriāli</span>
-            <span>Santehnika</span>
-            <span>Dārza tehnika</span>
-            <span>Pārvadāšana un iekraušana</span>
-            <span>Ģeneratori</span>
-            <span>Mazgāšanas aprīkojums</span>
-            <span>Mērinstrumenti</span>
-            <span>Mazgāšanas aprīkojums</span>
+            <span>Sieviešu apģērbi</span>
+            <span>Vīriešu apģērbi</span>
+            <span>Sieviešu apavi</span>
+            <span>Vīriešu apavi</span>
+            <span>Aksesuāri</span>
+            <span>Kostīmi</span>
+            <span>Sieviešu somiņas</span>
+            <span>Mugursomas un Čemodāni</span>
             <span>Un vēl...</span>
           </span>
         </div>
@@ -79,14 +79,14 @@ function BottomHome() {
           <span className='infoSVG2'><img src='/svg_baby.svg' alt='Baby icon'/></span>
           <span className='infoTitle2'>Bērni</span>
           <span className='infoText2'>
-            <span>Būvmateriāli</span>
-            <span>Santehnika</span>
-            <span>Dārza tehnika</span>
-            <span>Pārvadāšana un iekraušana</span>
-            <span>Ģeneratori</span>
-            <span>Mazgāšanas aprīkojums</span>
-            <span>Mērinstrumenti</span>
-            <span>Mazgāšanas aprīkojums</span>
+            <span>Meiteņu apģērbs</span>
+            <span>Zēnu apģērbs</span>
+            <span>Meiteņu apavi</span>
+            <span>Zēnu apavi</span>
+            <span>Aksesuāri</span>
+            <span>Rati un ķengursomas</span>
+            <span>Zīdaiņu gultas</span>
+            <span>Rotaļlietas</span>
             <span>Un vēl...</span>
           </span>
         </div>
@@ -94,14 +94,14 @@ function BottomHome() {
           <span className='infoSVG2'><img src='/svg_home.svg' alt='Home and Hobbies icon'/></span>
           <span className='infoTitle2'>Māja, hobijs</span>
           <span className='infoText2'>
-            <span>Būvmateriāli</span>
-            <span>Santehnika</span>
+            <span>Mēbeles</span>
+            <span>Sadzīves tehnika</span>
             <span>Dārza tehnika</span>
-            <span>Pārvadāšana un iekraušana</span>
-            <span>Ģeneratori</span>
-            <span>Mazgāšanas aprīkojums</span>
-            <span>Mērinstrumenti</span>
-            <span>Mazgāšanas aprīkojums</span>
+            <span>Dārza mēbeles un aksesuāri</span>
+            <span>Trauki, galda rīki</span>
+            <span>Aizkari un Paklāji</span>
+            <span>Sporta aprīkojums</span>
+            <span>Medības, kempings</span>
             <span>Un vēl...</span>
           </span>
         </div>
