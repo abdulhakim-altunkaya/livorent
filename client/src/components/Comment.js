ce909f2844ecfdc146b70d5aa9c230f10d357e7e
abdulhakim-altunkaya <altunkaya2@gmail.com>
2025-06-05 18:32:28 +0300

    second of part comment logic finished

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -4,17 +4,36 @@ import "../styles/Comment.css";
 
 function Comment() {
     const [textComment, setTextComment] = useState("");
+    const [isSaving, setIsSaving] = useState(false);
 
-    const saveComment = async (e) => {
-      setTestArea(textComment)
-    }
+    const saveComment = async () => {
+        if (textComment.length > 3000 || textComment.length < 4) {
+            alert("comment is too short or too long");
+            return;
+        }
+        setIsSaving(true);
+        try {
+            const commentObject = {
+                userComment : textComment,
+            }
+            // Replace with your API endpoint
+            await axios.post('http://localhost:5000/api/post/save-comment', commentObject);
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Failed to save comment.');
+        } finally {
+            setIsSaving(false);
+        }
+    };
 
     return (
         <div>
             <div className='commentArea'>
                 <textArea  className='commentInputText' type='text' placeholder="Comment or Question"
                     onChange={ (e) => setTextComment(e.target.value)} value={textComment} />
-                <button className='commentSaveBtn' onClick={saveComment}>Save</button>
+                <button className='commentSaveBtn' onClick={saveComment} disabled={isSaving} >
+                    {isSaving ? "Saglabā..." : "Saglabāt"}
+                </button>
             </div>
         </div>
     )
